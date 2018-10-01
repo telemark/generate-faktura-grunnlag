add682a54c0971a5abf5e75791b167b407d37f5f
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-10-01 13:37:54 +0200

    Adds shortening of long names (patch)

---
+++ tools/find-long-names.js
@@ -0,0 +1,11 @@
+(async () => {
+  const { readFile } = require('fs').promises
+  const filePath = 'test/directories/jobs/kravs-2018-2019.csv'
+  const data = await readFile(filePath, 'latin1')
+  data.split('\r\n').forEach(line => {
+    const list = line.split(';')
+    if (list[1].length > 40) {
+      console.log(list[1])
+    }
+  })
+})()
